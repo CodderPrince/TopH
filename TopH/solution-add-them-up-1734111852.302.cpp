86777002d4bed5ac543a062290c3d57d093a7de3
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-16 00:32:48 +0600

    Create solution-add-them-up-1734111852.302.cpp

---
+++ TopH/solution-add-them-up-1734111852.302.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int main() {
+    ll a, b;
+    cin >> a >> b;
+    cout << a + b << nl;
+
+    return 0;
+}
